757e767a73873d1fcc7f7e6288d33ad60929b403
Curtis Scott <curtis@curtisscott.com>
2018-03-08 13:43:41 -0500

    removed css-copy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const autoprefixer = require('autoprefixer');
-const browserSync 	= require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const cssnano = require('cssnano');
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
@@ -18,7 +18,7 @@ gulp.task('styles', function () {
     return gulp.src('src/assets/scss/*.scss')
         .pipe(sass())
         .pipe(postcss(plugins))
-        .pipe(gulp.dest('src/assets/styles'))
+        .pipe(gulp.dest('dist/assets/styles'))
         .pipe(browserSync.stream());
 
 });
@@ -28,10 +28,10 @@ gulp.task('copy-html', function(){
         .pipe(gulp.dest('dist'));
 });
 // copy all css files
-gulp.task('copy-css', function(){
-    gulp.src('src/assets/styles/*.css')
-        .pipe(gulp.dest('dist/assets/styles/'));
-});
+// gulp.task('copy-css', function(){
+//     gulp.src('src/assets/styles/*.css')
+//         .pipe(gulp.dest('dist/assets/styles/'));
+// });
 // image compression
 gulp.task('copy-compress-images', () =>
     gulp.src('src/assets/images/*')
@@ -47,7 +47,7 @@ gulp.task('concat-scripts', function(){
         .pipe(gulp.dest('src/assets/scripts'))
 });
 // watch and serve
-gulp.task('serve', ['styles'], function(){
+gulp.task('serve', function(){
 	browserSync.init({
 		server: './dist'
 	});
@@ -58,7 +58,7 @@ gulp.task('serve', ['styles'], function(){
 
 // build for dist
 gulp.task('build', function (done) {
-    sequence('styles','copy-html','copy-css', 'copy-compress-images', 'concat-scripts', done);
+    sequence('styles','copy-html', 'copy-compress-images', 'concat-scripts', done);
 });
 
 // default
